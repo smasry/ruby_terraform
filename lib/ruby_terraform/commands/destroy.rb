400990460c8b18c753d43a752073a6b4c9ede875
Samer Masry <samer.masry@gmail.com>
2018-10-24 16:58:16 -0700

    Adding lock option to destroy

--- lib/ruby_terraform/commands/destroy.rb
+++ lib/ruby_terraform/commands/destroy.rb
@@ -14,6 +14,7 @@ module RubyTerraform
         no_backup = opts[:no_backup]
         backup = no_backup ? '-' : opts[:backup]
         no_color = opts[:no_color]
+        no_color = opts[:lock]
 
         builder
             .with_subcommand('destroy') do |sub|
@@ -28,6 +29,7 @@ module RubyTerraform
               sub = sub.with_option('-backup', backup) if backup
               sub = sub.with_flag('-no-color') if no_color
               sub = sub.with_flag('-force') if force
+              sub = sub.with_option('-lock', lock) if lock
               sub
             end
             .with_argument(directory)
